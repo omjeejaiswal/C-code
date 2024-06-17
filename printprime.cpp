b1e32db22d736c61befbb9142b249344cd226be5
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-06-17 20:40:45 +0530

    printprime

---
+++ printprime.cpp
@@ -0,0 +1,23 @@
+// print all the no. b/w a and b
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+
+    for(int num=a; num<= b; num++){
+        int i;
+        for(i=2; i<num; i++){
+            if(num%i==0){
+                break;
+            }
+        }
+        if(i==num){
+            cout<<num<<endl;
+        }
+        
+    }
+return 0;
+}
