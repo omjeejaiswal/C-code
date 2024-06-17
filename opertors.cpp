50d66e04bc77440add12146a5226e3419cfb9471
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-06-17 20:39:27 +0530

    opeartor

---
+++ opertors.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int i=10, j=20, k;
+
+    //i = i++ + ++i;
+
+
+    //i = i++ - --i + ++i - i--;
+
+    k = i-- - i++ + --j - ++j + --i - j-- + ++i - j++;
+
+
+
+
+    cout<<i<<endl;
+    cout<<j<<endl;
+    cout<<k<<endl;
+
+    return 0;
+}
