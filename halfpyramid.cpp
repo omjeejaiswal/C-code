d27225d653f46dbf8afbd4be92854ca5cfeeb609
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-06-17 20:37:08 +0530

    halfPuramid

---
+++ halfpyramid.cpp
@@ -0,0 +1,15 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+     
+    int n;
+    //cout<<"Enter Number of HALF PYRAMID 180 ROTATION";
+    cin>>n;
+     for(int i=1; i<=n; i++){
+         for(int j=1; j<=1; j++){
+             cout<<i<<" ";
+         } cout<<endl;
+     }
+    return 0;
+}
