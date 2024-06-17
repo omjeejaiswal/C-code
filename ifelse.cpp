4cb4c7eb03a33afe95c95455f824b95fcffffe93
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-06-17 20:37:29 +0530

    ifelse

---
+++ ifelse.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int saving;
+    cin>>saving;
+
+    if(saving > 5000 ){
+    cout << "neha \n";
+    }
+    else if(saving >2000){
+        cout << "rashmi \n";
+    }
+    else {
+        cout << "Freinds \n";
+    }
+    return 0;
+}
