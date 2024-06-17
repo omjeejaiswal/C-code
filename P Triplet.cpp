a2c55f35d70448e321056800426e6564125f8c9b
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-06-17 20:39:46 +0530

    P triplet

---
+++ P Triplet.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+bool check(int x, int y,int z ){
+    int a = max(x, max(y,z));
+    int b,c;
+    if(a == x){
+        b=y;
+        c=z;
+    } 
+    else if(a == y){
+        b=x;
+        c=z;
+    }
+    else{
+        b=x;
+        c=z;
+    }
+    if(a*a == b*b + c*c)
+    return true;
+    else
+    return  false;
+}
+
+int main(){
+    int x,y,z;
+    cin>>x>>y>>z;
+
+    if(check(x,y,z)){
+        cout<<"pythagoran riplet";
+    }
+    else{
+        cout<<"not a pythagoran riplet";
+    }
+    return 0;
+}
